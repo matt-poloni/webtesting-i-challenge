be8feae6d8b6d6f3f0996026f3597330d622f2df
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-13 15:46:20 -0400

    Should complete repair() test & basic function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,10 +1,16 @@
-const enhancer = require('./enhancer.js');
+const {
+  succeed,
+  fail,
+  repair,
+  get,
+} = require('./enhancer.js');
 // test away!
 
 describe('enhancer.js', () => {
   describe('repair() method', () => {
-    it('does something', () => {
-      expect(true).toBe(true);
+    it('restores durability to 100', () => {
+      const item = { durability: 0 };
+      expect(repair(item).durability).toBe(100);
     })
   })
 })

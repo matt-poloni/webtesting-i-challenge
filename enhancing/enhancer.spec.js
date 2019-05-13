ae523a0445930ccfd75c4e4da806a4e8f0fcead6
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-13 15:26:11 -0400

    Adds & initializes jest, populates dummy test in spec file

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+  describe('repair() method', () => {
+    it('does something', () => {
+      expect(true).toBe(true);
+    })
+  })
+})
